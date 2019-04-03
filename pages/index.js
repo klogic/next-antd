d679dfaf803510090d43de6d09f0cd104c03cb35
Narongsak Keawmanee <klogic@hotmail.co.th>
2019-04-03 07:33:01 +0700

    init

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { DatePicker, Button } from "antd";
+export default () => (
+  <div>
+    <DatePicker />
+    <Button type="primary">Primary</Button>
+    <Button>Default</Button>
+    <Button type="dashed">Dashed</Button>
+    <Button type="danger">Danger</Button>
+    Hello, World!
+  </div>
+);
